dc86d0ec2e86c10509ea3c4214e570143f2ff5c2
Diogo Trajano <diogotrajano@gmail.com>
2016-07-20 20:02:17 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# KillTasksButton README
+
+Kill tasks button is a button located at the bottom status bar, in order to facilitate the completion of tasks running
+
+## Features
+
+kill tasks button is basically a shortcut to native functionality of visual studio code 'Terminate Running tasks' 
+
+ Button Terminate Running Task:
+ 
+![alt tag](/Users/dtrajano/Documents/Workspaces/VisualStudioCodeExtensions/KillTasksButton/src/images/killTaskButton.png)
+
+Native functionality 'Terminate running tasks'
+
+![alt tag](/Users/dtrajano/Documents/Workspaces/VisualStudioCodeExtensions/KillTasksButton/src/images/nativeTerminateRunTasks.png)
+
+### For more information and contribute code
+
+* [Kill Task Button(GitHub)](http://code.visualstudio.com/docs/languages/markdown)
---
+++ src/extension.ts
@@ -0,0 +1,38 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the necessary extensibility types to use in your code below
+import * as vscode from 'vscode';
+import {window, commands, Disposable, ExtensionContext, StatusBarAlignment, StatusBarItem, TextDocument} from 'vscode';
+
+// This method is called when your extension is activated. Activation is
+// controlled by the activation events defined in package.json.
+export function activate(context: ExtensionContext) {
+
+    // Use the console to output diagnostic information (console.log) and errors (console.error).
+    // This line of code will only be executed once when your extension is activated.
+    console.log('Congratulations, your extension "WordCount" is now active!');
+
+    // create a new word counter
+    let terminateTaskButtonInstance = new TerminateTask();
+
+    // Add to a list of disposables which are disposed when this extension is deactivated.
+    context.subscriptions.push(terminateTaskButtonInstance);
+}
+
+class TerminateTask {
+
+    private _statusBarItem: StatusBarItem;
+
+    constructor(){
+        // Create as needed
+        console.log("teste");
+        this._statusBarItem = window.createStatusBarItem(StatusBarAlignment.Right);
+        this._statusBarItem.text = `$(circle-slash) Terminate Running Tasks`;
+        //this._statusBarItem.command = "workbench.action.tasks.stop";
+        this._statusBarItem.command = "workbench.action.tasks.terminate";
+        this._statusBarItem.show();
+    }
+
+    dispose() {
+        this._statusBarItem.dispose();
+    }
+}
